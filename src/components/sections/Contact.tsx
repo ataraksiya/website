3ac9e7c87cb55c38711ec2841d879a446c4262e0
Tuna <barbeku@protonmail.com>
2025-01-04 11:27:39 +0300

    feat: made contact form responsive

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -58,7 +58,7 @@ export default function Contact() {
   };
 
   return (
-    <div className="col-span-2 grid w-full grid-cols-2 justify-center gap-3 rounded-lg border bg-card px-5 py-5 drop-shadow-2xl xl:col-span-4">
+    <div className="col-span-2 grid w-full grid-cols-1 justify-center gap-3 rounded-lg border bg-card px-5 py-5 drop-shadow-2xl xl:col-span-4 xl:grid-cols-2">
       <div className="flex flex-col gap-2">
         <div className="flex flex-row items-center gap-2">
           <Image
@@ -68,11 +68,13 @@ export default function Contact() {
             draggable={false}
             quality={100}
             alt="contact"
-            className="aspect-square"
+            className="aspect-square size-8 xl:size-10"
           />
-          <span className="text-3xl font-medium">Want to work together?</span>
+          <span className="text-xl font-medium xl:text-3xl">
+            Want to work together?
+          </span>
         </div>
-        <p className="text-lg text-muted">
+        <p className="text-base text-muted xl:text-lg">
           I&apos;m always open to discussing new projects, creative ideas, or
           opportunities to be part of your vision. Let&apos;s connect and
           explore how we can collaborate to create something amazing.
